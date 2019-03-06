a5ecca3bb0dd6d75ed6a8c6604340949cb28ac5d
Julien <finaly55@gmail.com>
2019-03-06 10:56:38 +0100

    Ajoutez des fichiers projet.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            int y;
+            x = int.Parse(Console.ReadLine());
+            y = int.Parse(Console.ReadLine());
+
+            Utile util = new Utile();
+            Console.WriteLine(util.Add(x, y).ToString());
+            Console.ReadLine();
+        }
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Add_4_3_Retourne7()
+        {
+            ConsoleApp2.Utile util = new ConsoleApp2.Utile();
+
+            int x = 3;
+            int y = 4;
+            Assert.AreEqual(7, util.Add(x, y), "La valeur doit être égale à 7");
+        }
+
+        [TestMethod]
+        public void Add_4_3_Retourne8()
+        {
+            ConsoleApp2.Utile util = new ConsoleApp2.Utile();
+
+            int x = 3;
+            int y = 4;
+            Assert.AreEqual(8, util.Add(x, y), "La valeur doit être égale à 7");
+        }
+    }
+}
